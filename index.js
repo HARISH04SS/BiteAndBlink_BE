e25244c214b517d24d917f3fa827ad7887164b38
harish04ss <harishsubramanian4122001@gmail.com>
2025-01-11 18:34:21 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const config = require('config');
-mongoose.connect()
+const {mongodb_URL} = require('./utils/config');
+mongoose.connect(mongodb_URL)
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB...', err));
